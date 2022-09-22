538e85bed926a8ade40a4e0b49e2886976783871
Anthony Spiniello <fiskou2003@gmail.com>
2022-09-22 08:24:08 +0200

    Mise en ligne de l'API

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -8,7 +8,7 @@ import axios from 'axios'
 // "export default () => {}" function below (which runs individually
 // for each client)
 const api = axios.create({
-  baseURL: 'http://localhost:8000/',
+  baseURL: 'https://api-cafe.divtec.me/',
   // timeout: 5000,
   headers: {
     Accept: 'application/json',
