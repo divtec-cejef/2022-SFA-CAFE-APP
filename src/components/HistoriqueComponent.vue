4fad7c8ef7f56473d0cb7aaf1ed5fd5b39bfb0e7
Anthony Spiniello <fiskou2003@gmail.com>
2022-09-22 08:23:12 +0200

    Ajout des notifications et de la suppression concernant les transactions

--- src/components/HistoriqueComponent.vue
+++ src/components/HistoriqueComponent.vue
@@ -19,16 +19,52 @@
       <q-separator vertical />
 
       <q-card-actions vertical class="justify-around q-px-md col-2">
-        <q-btn flat round color="red" icon="delete" />
+        <q-btn flat round color="red" icon="delete" @click="confirm = true"/>
+        <q-dialog v-model="confirm" >
+          <q-card>
+            <q-card-section class="row items-center">
+              <q-avatar icon="delete" color="primary" text-color="white" />
+              <span class="q-ml-sm">Êtes-vous sûr de vouloir supprimer cette transaction ?</span>
+            </q-card-section>
+
+            <q-card-actions align="right">
+              <q-btn flat label="Annuler" color="black" v-close-popup />
+              <q-btn flat label="Supprimer" color="red" v-close-popup @click="removeTransaction"/>
+            </q-card-actions>
+          </q-card>
+        </q-dialog>
       </q-card-actions>
     </q-card-section>
   </q-card>
 </template>
 
 <script>
+import { mapActions, mapGetters } from 'vuex'
+import { useQuasar } from 'quasar'
+
 export default {
   name: 'HistoriqueComponent',
+  data () {
+    return {
+      confirm: false
+    }
+  },
+  setup () {
+    const $q = useQuasar()
+    return {
+      showNotif () {
+        $q.notify({
+          message: this.setNotification.message,
+          color: this.setNotification.color
+        })
+      }
+    }
+  },
   props: {
+    id: {
+      type: Number,
+      required: true
+    },
     type: {
       type: String,
       required: true
@@ -47,11 +83,24 @@ export default {
     },
     quantite: Number
   },
-  data () {
-    return {
+  methods: {
+    ...mapActions('userStore', ['supprimerTransaction', 'getHistorique', 'getUserSolde']),
+    async removeTransaction () {
+      console.log(this.id)
+      console.log(this.type)
+      await this.supprimerTransaction({
+        id: this.id, type: this.type
+      })
+      this.getHistorique()
+      await this.getUserSolde()
+      this.showNotif()
     }
   },
-  methods: {
+  computed: {
+    ...mapGetters('userStore', ['getNotification']),
+    setNotification () {
+      return this.getNotification
+    }
   }
 }
 </script>
